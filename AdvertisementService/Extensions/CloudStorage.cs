274d68db33a0ae6d4c25f429d0321377b2f5bfa4
Ahmer.Khalid <a.khalid@routesme.com>
2023-01-18 19:06:38 +0500

    Improve the bugfix in advertisement,campaign and media DAL files

--- AdvertisementService/Extensions/CloudStorage.cs
+++ AdvertisementService/Extensions/CloudStorage.cs
@@ -1,5 +1,6 @@
 ﻿using AdvertisementService.Models.Common;
 using AdvertisementService.Models.DBModels;
+using AdvertisementService.Models.Dtos;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
 using System;
@@ -12,7 +13,7 @@ namespace AdvertisementService.Extensions
     public static class CloudStorage
     {
         static string blobUrl = string.Empty;
-        internal static async Task<string> UploadImageToCloudReturnUrlAsync(Medias media, AzureStorageBlobConfig _config)
+        internal static async Task<string> UploadImageToCloudReturnUrlAsync(PostMediaDto media, AzureStorageBlobConfig _config)
         {
             string mediaReferenceName = media.MediaFile.FileName.Split('.')[0] + "_" + DateTime.UtcNow.Ticks + "." + media.MediaFile.FileName.Split('.')[1];
 
@@ -27,16 +28,16 @@ namespace AdvertisementService.Extensions
             return blobUrl;
         }
 
-        internal static async Task<string> UploadVideoToCloudReturnUrlAsync(Medias media, AzureStorageBlobConfig _config)
+        internal static async Task<string> UploadVideoToCloudReturnUrlAsync(PostMediaDto media, AzureStorageBlobConfig _config)
         {
-            var videoPath = string.Empty; //await _videoConversionRepository.ConvertVideoAsync(model.media, model.Mute);
-            string mediaReferenceName = videoPath.Split("\\").LastOrDefault();
+            
+            string mediaReferenceName = media.MediaFile.FileName.Split("\\").LastOrDefault();
             if (CloudStorageAccount.TryParse(_config.StorageConnection, out CloudStorageAccount storageAccount))
             {
                 CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                 CloudBlobContainer container = blobClient.GetContainerReference(_config.Container);
                 CloudBlockBlob blockBlob = container.GetBlockBlobReference(mediaReferenceName);
-                await blockBlob.UploadFromStreamAsync(File.OpenRead(videoPath));
+                await blockBlob.UploadFromStreamAsync(File.OpenRead(mediaReferenceName));
                 blobUrl = blockBlob.Uri.AbsoluteUri;
 
             }
---
+++ AdvertisementService/Models/Dtos/PostMediaDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using AdvertisementService.Models.DBModels;
+
+namespace AdvertisementService.Models.Dtos
+{
+    public class PostMediaDto
+    {
+        public int MediaId { get; set; }
+        public string Url { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public MediaType? MediaType { get; set; }
+        public int? MediaMetadataId { get; set; }
+        [NotMappedAttribute]
+        public IFormFile MediaFile { get; set; }
+        public float? Size { get; set; }
+        public float? Duration { get; set; }
+    }
+   
+}
