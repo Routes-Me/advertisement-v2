ec9aed515b75270e00ff1abebddbeccd0ff185d7
Ahmer.Khalid <a.khalid@routesme.com>
2023-01-20 14:59:01 +0500

    Update the AdvertisementsReportController and ContentsController in this branch

--- AdvertisementService/Controllers/ContentsController.cs
+++ AdvertisementService/Controllers/ContentsController.cs
@@ -1,9 +1,14 @@
 ﻿using AdvertisementService.Abstraction;
 using AdvertisementService.DAL;
+using AdvertisementService.Models;
 using AdvertisementService.Models.Common;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using static AdvertisementService.Models.Response;
 
 namespace AdvertisementService.Controllers
 {
@@ -16,31 +21,44 @@ namespace AdvertisementService.Controllers
         private readonly IUnitOfWork _unitOfWork;
         private readonly AppSettings _appSettings;
         private readonly IMapper _mapper;
-        private readonly CampaignsDAL _campaignsDAL;
 
 
-        public ContentsController(IUnitOfWork unitOfWork, IOptions<Dependencies> dependencies, IOptions<AppSettings> appSettings, IMapper mapper, CampaignsDAL campaignsDAL)
+        public ContentsController(IUnitOfWork unitOfWork, IOptions<Dependencies> dependencies, IOptions<AppSettings> appSettings, IMapper mapper )
         {
             _unitOfWork = unitOfWork;
             _dependencies = dependencies.Value;
             _appSettings = appSettings.Value;
             _mapper = mapper;
-            _campaignsDAL = campaignsDAL;
         }
 
 
-        //[HttpGet]
-        //public async Task<IActionResult> GetContent([FromQuery] Pagination pagination)
-        //{
-        //    try
-        //    {
-        //        var getContentDto = await new ContentsDAL(_unitOfWork, _dependencies, _appSettings, _mapper, _campaignsDAL).GetContent(pagination);
-        //        return StatusCode(getContentDto.Code, getContentDto);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return StatusCode(StatusCodes.Status400BadRequest, ReturnResponse.ExceptionResponse(e));
-        //    }
-        //}
+        [HttpGet]
+        public async Task<IActionResult> GetContent([FromQuery] Pagination pagination)
+        {
+            try
+            {
+                var getContentDto = await new ContentsDAL(_unitOfWork, _dependencies, _appSettings, _mapper).GetContent(pagination);
+                return StatusCode(getContentDto.Code, getContentDto);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ReturnResponse.ExceptionResponse(e));
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public  IActionResult GetById(string id)
+        {
+            try
+            {
+                var response =  new ContentsDAL(_unitOfWork, _dependencies, _appSettings, _mapper).GetByIdContent(id);
+                return StatusCode(response.Code, response);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ReturnResponse.ExceptionResponse(ex));
+            }
+        }
     }
 }
--- AdvertisementService/Extensions/APIExtensions.cs
+++ AdvertisementService/Extensions/APIExtensions.cs
@@ -78,44 +78,44 @@ namespace AdvertisementService.Extensions
 
             return JArray.Parse(JsonConvert.SerializeObject(promotions.GroupBy(x => x.PromotionId).Select(x => x.First()).ToList()));
         }
-        //public static List<PromotionsGetModel> GetPromotionsContents(List<ContentReadDto> contents, string url)
-        //{
-        //    List<PromotionsGetModel> promotions = new List<PromotionsGetModel>();
-        //    List<int> id = new List<int>(contents.Select(x => Obfuscation.Decode(x.ContentId)).ToList().Distinct());
+        public static List<GetPromotionDto> GetPromotionsContents(List<GetContentDto> contents, string url)
+        {
+            List<GetPromotionDto> promotions = new List<GetPromotionDto>();
+            List<int> id = new List<int>(contents.Select(x => Obfuscation.Decode(x.ContentId)).ToList().Distinct());
 
-        //    var client = new RestClient(url);
-        //    client.Timeout = -1;
-        //    var request = new RestRequest(Method.POST);
-        //    request.AddParameter("application/json; charset=utf-8", JsonConvert.SerializeObject(id), ParameterType.RequestBody);
-        //    IRestResponse response = client.Execute(request);
-        //    if (response.StatusCode == HttpStatusCode.OK)
-        //    {
-        //        var result = response.Content;
-        //        var promotionsData = JsonConvert.DeserializeObject<GetResponse<PromotionsGetModel>>(result);
-        //        promotions.AddRange(promotionsData.data);
-        //    }
-        //    else
-        //    {
-        //        throw new Exception(response.ErrorException.ToString());
-        //    }
-        //    return promotions;
-        //}
-        //public static PromotionsGetModel GetPromotionsContentById(ContentReadDto content, string url)
-        //{
-        //    PromotionsGetModel promotion = new PromotionsGetModel();
-        //    List<int> id = new List<int>();
-        //    id.Add(Obfuscation.Decode(content.ContentId));
-        //    var client = new RestClient(url);
-        //    var request = new RestRequest(Method.POST);
-        //    request.AddParameter("application/json; charset=utf-8", JsonConvert.SerializeObject(id), ParameterType.RequestBody);
-        //    IRestResponse response = client.Execute(request);
-        //    if (response.StatusCode == HttpStatusCode.OK)
-        //    {
-        //        var result = response.Content;
-        //        var promotionData = JsonConvert.DeserializeObject<GetResponseApi<PromotionsGetModel>>(result);
-        //        promotion = promotionData.data[0];
-        //    }
-        //    return promotion;
-        //}
+            var client = new RestClient(url);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("application/json; charset=utf-8", JsonConvert.SerializeObject(id), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = response.Content;
+                var promotionsData = JsonConvert.DeserializeObject<GetResponse<GetPromotionDto>>(result);
+                promotions.AddRange(promotionsData.Data);
+            }
+            else
+            {
+                throw new Exception(response.ErrorException.ToString());
+            }
+            return promotions;
+        }
+        public static GetPromotionDto GetPromotionsContentById(GetContentDto content, string url)
+        {
+            GetPromotionDto promotion = new GetPromotionDto();
+            List<int> id = new List<int>();
+            id.Add(Obfuscation.Decode(content.ContentId));
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("application/json; charset=utf-8", JsonConvert.SerializeObject(id), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = response.Content;
+                var promotionData = JsonConvert.DeserializeObject<GetResponseApi<GetPromotionDto>>(result);
+                promotion = promotionData.Data[0];
+            }
+            return promotion;
+        }
     }
 }
